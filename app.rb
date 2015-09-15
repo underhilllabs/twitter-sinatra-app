0eff51116efc47ec43f260c875efa400da2c1969
Bart Lantz <underhilllabs@gmail.com>
2015-09-15 22:04:56 +0000

    initial commit

---
+++ app.rb
@@ -0,0 +1,26 @@
+require 'rubygems'
+require 'sinatra/base'
+require 'twitter'
+require 'dotenv'
+Dotenv.load
+
+class MyApp < Sinatra::Base
+  before do 
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["consumer_key"]
+      config.consumer_secret     = ENV["consumer_secret"]
+      config.access_token        = ENV["access_token"]
+      config.access_token_secret = ENV["access_token_secret"]
+    end
+  end
+
+  get "/" do
+    followers = @client.followers
+    friends = @client.following
+    follower_ids = followers.collect { |f| f.id }
+    friends_ids = friends.collect { |f| f.id }
+    @nonfollowers = friends.reject{|f| follower_ids.include? f.id }
+    @nonfollowing = followers.reject{|f| friends_ids.include? f.id }
+    erb :index
+  end
+end
